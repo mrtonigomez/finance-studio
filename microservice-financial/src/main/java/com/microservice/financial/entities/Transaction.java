ea369d5bde1f2da06e102c9211ae2d73445b1bb6
Antoni Gomez <tonigomezjuan@gmail.com>
2025-10-02 11:11:54 +0200

    Modify Transaction entity

--- microservice-financial/src/main/java/com/microservice/financial/entities/Transaction.java
+++ microservice-financial/src/main/java/com/microservice/financial/entities/Transaction.java
@@ -29,6 +29,8 @@ public class Transaction {
     @Column(nullable = false)
     private Float quantity;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
     private Category category;
 
     private String notes;
